9ad1c59503d31f006d716fe38e928a52f58a1e70
benjvv19 <ben.camus19@gmail.com>
2024-11-16 15:12:32 -0400

    Se arreglaron los errores de el spect de las categorias

--- src/app/pages/categorias/categorias.page.spec.ts
+++ src/app/pages/categorias/categorias.page.spec.ts
@@ -1,11 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CategoriasPage } from './categorias.page';
+import { NativeStorage } from '@awesome-cordova-plugins/native-storage/ngx';
+import { ServicebdService } from 'src/app/services/servicebd.service';
+import { Router } from '@angular/router';
+import { SQLite } from '@awesome-cordova-plugins/sqlite/ngx';
+
 
 describe('CategoriasPage', () => {
   let component: CategoriasPage;
   let fixture: ComponentFixture<CategoriasPage>;
 
-  beforeEach(() => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [CategoriasPage],
+      providers: [
+        { provide: ServicebdService},
+        { provide: SQLite}, 
+        { provide: NativeStorage}, 
+        { provide: Router, useValue: { navigate: jasmine.createSpy() } }
+      ]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(CategoriasPage);
     component = fixture.componentInstance;
     fixture.detectChanges();
