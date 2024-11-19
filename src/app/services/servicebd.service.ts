f2848c7d0d1681a0a42bfe1a86529da3c518312b
benjvv19 <ben.camus19@gmail.com>
2024-11-18 22:57:11 -0400

    a

--- src/app/services/servicebd.service.ts
+++ src/app/services/servicebd.service.ts
@@ -184,7 +184,7 @@ registroZapatillas: string = `
       await this.database.executeSql('DROP TABLE IF EXISTS ventas', []);
       await this.database.executeSql('DROP TABLE IF EXISTS detalle_ventas', []);
       await this.database.executeSql('DROP TABLE IF EXISTS zapatillas', []);
-      //await this.database.executeSql('DROP TABLE IF EXISTS usuario', []);
+      await this.database.executeSql('DROP TABLE IF EXISTS usuario', []);
       await this.database.executeSql('DROP TABLE IF EXISTS categoria_zapatillas', []);
 
       await this.database.executeSql(this.tablaRoles, []);
